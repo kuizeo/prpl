25b79afa97e607bd93dcc5d63f50d22e72695169
kuizeo <kuizeo@gmail.com>
2021-05-16 11:14:12 -0400

    initial commit

---
+++ prpl.js
@@ -0,0 +1,15 @@
+function prpl(promise) {
+  // PRPL: PRomises PLus™
+
+  return {
+    // promise-ish object, I guess
+
+    then: (...extra_args) => {
+      // calls first argument to .then with arguments to resolve & other args
+      promise.then((...args) => extra_args[0](...args, ...extra_args.slice(1)));
+    },
+
+    // other methods of a promise
+    ...promise,
+  };
+}
